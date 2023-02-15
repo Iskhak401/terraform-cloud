5a01285cc5434522c187becb42d18eff2124e795
Iskhak401 <moldokasymoviskhak@gmail.com>
2023-02-15 20:25:53 +0600

    asdf

--- cloud-log.tf
+++ cloud-log.tf
@@ -1,62 +1,62 @@
-# resource "aws_flow_log" "flow-role" {
-#   iam_role_arn    = aws_iam_role.flow-role.arn
-#   log_destination = aws_cloudwatch_log_group.vpc-logs.arn
-#   traffic_type    = "ALL"
-#   vpc_id          = aws_vpc.some_custom_vpc.id
-# }
+resource "aws_flow_log" "flow-role" {
+  iam_role_arn    = aws_iam_role.flow-role.arn
+  log_destination = aws_cloudwatch_log_group.vpc-logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.some_custom_vpc.id
+}
 
 
-# resource "aws_cloudwatch_log_group" "vpc-logs" {
-#   name = "vpc-logs"
+resource "aws_cloudwatch_log_group" "vpc-logs" {
+  name = "vpc-logs"
 
-#   tags = {
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Environment = "development"
+  }
+}
 
 
-# resource "aws_iam_role" "flow-role" {
-#   name = "example-role"
+resource "aws_iam_role" "flow-role" {
+  name = "example-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole",
-#         Effect = "Allow",
-#         Principal = {
-#           Service = [
-#             "logs.amazonaws.com",
-#             "vpc-flow-logs.amazonaws.com"
-#           ]
-#         }
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = [
+            "logs.amazonaws.com",
+            "vpc-flow-logs.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_policy" "my-policy" {
-#   name = "example-policy"
+resource "aws_iam_policy" "my-policy" {
+  name = "example-policy"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = [
-#           "logs:CreateLogGroup",
-#           "logs:CreateLogStream",
-#           "logs:PutLogEvents",
-#           "logs:DescribeLogGroups",
-#           "logs:DescribeLogStreams"
-#         ],
-#         Effect   = "Allow",
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "example_policy_attachment" {
-#   policy_arn = aws_iam_policy.my-policy.arn
-#   role       = aws_iam_role.flow-role.name
-# }
+resource "aws_iam_role_policy_attachment" "example_policy_attachment" {
+  policy_arn = aws_iam_policy.my-policy.arn
+  role       = aws_iam_role.flow-role.name
+}
